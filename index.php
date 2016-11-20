2ca900e4c5a5c1ae3fb9b288dcb4ccb37051ad39
ryan sharkey <ryan sharkey>
2016-11-20 17:04:04 -0500

    modified index.html, styles, and header pages

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Ithaca Applefest</title>
+    <link rel="stylesheet" type="text/css" href="styles/styles.css">
+  </head>
+  <body>
+    <div id="title">
+      <h1>Ithaca Applefest</h1>
+    </div>
+    <?php include "includes/header.php";?>
+    <div id="container">
+    </div>
+    <footer>
+      ITHACA APPLE FEST
+    </footer>
+  </body>
+</html>
