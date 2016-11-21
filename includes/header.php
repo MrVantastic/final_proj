79ee638da30b726ba38d6b1d77873683eb0f3f3a
ryan sharkey <ryan sharkey>
2016-11-20 22:16:23 -0500

    updated header/styles

---
+++ includes/header.php
@@ -0,0 +1,6 @@
+    <nav>
+      <a href="index.php">Home</a>
+      <a href="kids.php">Kids Zone</a>
+      <a href="planyourtrip.html">Plan Your Trip</a>
+      <a href="vendordetails.html"> Vendor details</a>
+    </nav>
