7cac3399e5c5007841f9912605c5adccb19ec59a
Jeffrey Van <Jeff@dhcp-rhodes-1220.eduroam.cornell.edu>
2016-12-08 10:10:30 -0500

    fixed fonts and added description to home page

--- index.php
+++ index.php
@@ -19,6 +19,11 @@
         <!--apple_fest3 taken from https://i.ytimg.com/vi/L8dSbuQkY48/maxresdefault.jpg-->
     <a class="prev" onclick="pic_rotate(-1)">&#10094;</a>
     <a class="next" onclick="pic_rotate(1)">&#10095;</a>
+    <br />
+    <br />
+    <h2>Welcome to Ithaca's Applefest</h2>
+    <p>The Ithaca Applefest is an annual celebration of the Upstate New York apple season. Here you can find plenty of apples, local produce, fresh baked goods, family entertainment at every turn, games, rides, prizes and live performances on two stages. Applefest is a great introduction to the rich farm and artist community of Ithaca.  First time visitors can witness a variety of wonderful local entertainment while also visiting the historical outdoor pedestrian shopping mall, The Ithaca Commons.  The pedestrian mall is the epicenter of the festival and the gateway to a nostalgic journey to harvest season fun.
+</p>
     </div>
     <footer>
       ITHACA APPLE FEST
--- vendordetails.php
+++ vendordetails.php
@@ -13,18 +13,18 @@
       <h1>Applefest Vendor Details</h1>
     </div>
     <?php include "includes/header.php";?>
-    
+
     <div id="container">
-    
-        <div id="content"> 
-            
+
+        <div id="content">
+
             <div id="center">
                 <h2>Map</h2>
                 <div id="image">
                     <img src="images/map.jpg" alt="Applefest Map"> <!-- Citation URL: http://www.downtownithaca.com/ithaca-events/Apple%20Harvest%20Festival%20Presented%20by%20Tompkins%20Trust -->
                     <p class="caption">Figure 1: Applefest Layout and Overview of Activities</p>
                 </div>
-                
+
                 <h2>List Of Vendors</h2>
                 <p>Every year, Applefest hosts a variety different vendors who provide fresh baked goods, family entertainment, local goods, and of course, endless apples.Over 100 vendors will be stationed around the Ithaca Commons, selling unique foods (like hot apple cider donuts, kettle corn), handcrafted goods, and other specialties. An abridged list of vendors includes:</p>
                 <ul>
@@ -34,12 +34,16 @@
                     <li>El Kacike's Caribbean Chicken</li>
                     <li>Gourmet Caramel Apples</li>
                     <li>Indian Creek Farm</li>
+                    <br />
                     <img src="images/vendor.jpg" alt="Indian Creek Farm">
                     <!-- Citation URL: http://ithacavoice.com/wp-content/uploads/2015/10/Screen-Shot-2015-10-05-at-8.32.29-AM.png -->
+                    <br />
+                    <br />
                     <li>Ithaca Farmers Market</li>
                     <li>Ithaca Hummus</li>
                     <li>Laughing Goat Fiber Farm</li>
                     <li>SK Henna</li>
+                    <br />
                     <img src="images/henna.jpg" alt="SK Henna">
                     <!-- Citation URL: https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/14448932_992255234218838_1805076727311184434_n.jpg?oh=8ef3b3fe9172c0dea255cb7327a263f3&oe=58F7FEDC -->
                 </ul>
@@ -47,9 +51,9 @@
             </div>
 
         </div>
-        
+
     </div>
-    
+
 </body>
-         
+
 </html>
